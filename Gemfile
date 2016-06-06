cf085b85997ffb85fbdd3cc0c60c2e912c5d6016
Stefan Haslinger <stefan.haslinger@mittenin.at>
2016-06-06 15:44:17 +0200

    missing episode 81

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source 'http://rubygems.org'
 gem 'jekyll-octopod', path: '../jekyll-octopod'
+group :jekyll_plugins do
+  gem 'octopress-debugger'
+end
--- _plugins/feed_generator.rb
+++ _plugins/feed_generator.rb
@@ -25,7 +25,6 @@ module Jekyll
 
     def generate(site)
       pages_total = (site.posts.docs.count.to_f / site.config["episodes_per_feed_page"]).ceil
-
       site.config["episode_feed_formats"].each do |page_format|
         name = "episodes." + page_format + ".rss"
         page = PagedFeedPage.new(site, site.source, ".", name, 1, pages_total, page_format)
