42d56ccf42cd64afc52482d8e9836b94766543ef
Stefan Haslinger <stefan.haslinger@informatom.com>
2016-08-31 21:01:53 +0200

    update to latest version

--- _plugins/podcast_player_page_generator.rb
+++ _plugins/podcast_player_page_generator.rb
@@ -13,6 +13,8 @@ module Jekyll
       self.data['subtitle'] = post.data['subtitle']
       self.data['datum']    = post.data['datum']
       self.data['author']   = post.data['author']
+      self.data['duration'] = post.data['duration']
+      self.data['summary']  = post.data['summary']
       self.data['explicit'] = post.data['explicit']
       self.data['audio']    = post.data['audio']
       self.data['chapters'] = post.data['chapters']
